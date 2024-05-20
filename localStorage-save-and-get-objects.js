5abd0d6f92ccd7f5f4f8e26cb77f35fbb1000b6e
Alejo-P <150528715+Alejo-P@users.noreply.github.com>
2024-05-19 21:54:21 -0500

    Updating of localStorage-save-and-get-objects.js file

--- localStorage-save-and-get-objects.js
+++ localStorage-save-and-get-objects.js
@@ -1,16 +1,18 @@
-// Guardar un elemento en el localStorage
-fetch("https://jsonplaceholder.typicode.com/posts/7") // Realizar una solicitud HTTP GET a la API
-  .then(response => response.json()) // Convertir la respuesta a JSON
+// Save an item in localStorage
+fetch("https://jsonplaceholder.typicode.com/posts/7") // Make an HTTP GET request to the API
+  .then(response => response.json()) // Convert the response to JSON
   .then(post => {
-    // Almacenar el JSON convertido en localStorage con la clave "post7"
+    // Store the converted JSON in localStorage with the key "post7"
     localStorage.setItem("post7", JSON.stringify(post));
-    // Imprimir el JSON en la consola
-    console.log(`El JSON obtenido es: ${JSON.stringify(post)}`);})
-  // En caso de errores en alguna de las promesas
-  .catch(error => console.log(`Ocurrió un error: ${error}`));
+    // Print the JSON to the console
+    console.log(`The obtained JSON is: ${JSON.stringify(post)}`);
+  })
+  // In case of errors in any of the promises
+  .catch(error => console.log(`An error occurred: ${error}`));
 
-// Obtener un elemento del localStorage
-// Recuperar la cadena JSON del localStorage accediendo con la clave "post7"
-let post = JSON.parse(localStorage.getItem("post7")); //Convertir la cadena a un objeto JavaScript
-// Imprimir el objeto JavaScript en la consola
+// Retrieve an item from localStorage
+// Retrieve the JSON string from localStorage using the key "post7"
+let post = JSON.parse(localStorage.getItem("post7")); // Convert the string to a JavaScript object
+// Print the JavaScript object to the console
 console.log(post);
+
